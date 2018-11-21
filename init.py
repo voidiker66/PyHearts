0aebb6a59423124c1c4c1a7af1f8a29d313da6ac
Austin Ikerd <asi32@utexas.edu>
2018-11-21 12:45:04 -0600

    only play once

--- init.py
+++ init.py
@@ -1,9 +1,8 @@
 from Game import Game, Player
 
-for i in range(1000):
-	game = Game(debug=True)
-	game.add_player(Player("Austin", debug=True))
-	game.add_player(Player("Alex", debug=True))
-	game.add_player(Player("Mom", debug=True))
-	game.add_player(Player("Dad", debug=True))
-	game.play()
+game = Game(debug=True)
+game.add_player(Player("Austin", debug=True))
+game.add_player(Player("Alex", debug=True))
+game.add_player(Player("Mom", debug=True))
+game.add_player(Player("Dad", debug=True))
+game.play()
