29191263ae39055943c667fe95eafd1510d422d6
Austin Ikerd <asi32@utexas.edu>
2018-11-27 09:53:04 -0600

    start of policy

--- SmartPlayer.py
+++ SmartPlayer.py
@@ -9,9 +9,24 @@ class SmartPlayer(Player):
 	"""
 
 	def take_turn(self, initial, first_card=None, hearts_broken=False):
-		print(self.tracker)
-		return super(SmartPlayer, self).take_turn(initial, first_card, hearts_broken)
+		# return super(SmartPlayer, self).take_turn(initial, first_card, hearts_broken)
+
+		# get the card with the max policy value in this situation
+		# need to fix because max() returns the value not the card
+		card_index = max(list(map(lambda v: self.policy(v), self.hand)))
+
+		# this is the card we should play according to our policy
+		card = self.hand.deck_array[card_index]
+		self.hand.remove_card(card_index)
+		return card
 
 
 	def add_tracker(self, tracker):
 		self.tracker = tracker
+
+	def policy(self, card):
+		"""
+			assigns a point value to the card based on the
+			potential benefits of playing the card in this situation
+		"""
+		return 1
